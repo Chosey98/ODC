8cb2e31359f751d22ae8a0ca576a2fceda3b064e
Yousef Ashraf <bosayousef981@gmail.com>
2022-04-20 11:21:00 +0200

    Added validation and message

--- AdminServer/Routes/admin/subadmins/add.js
+++ AdminServer/Routes/admin/subadmins/add.js
@@ -9,10 +9,35 @@ export default async (req, res) => {
 			message: 'email, password, name, role, phoneNumber are required',
 		});
 	}
-	if (!email.match(/@/g)) {
+	if (
+		!email.match(
+			/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		)
+	) {
 		return res.status(400).send({
 			success: false,
 			message: 'email is invalid',
+		});
+	}
+	if (!phoneNumber.match(/^[0-9]{11}$/)) {
+		return res.status(400).send({
+			success: false,
+			message: 'phoneNumber must be numeric and contains 11 digits',
+		});
+	}
+	if (
+		!password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)
+	) {
+		return res.status(400).send({
+			success: false,
+			message:
+				'password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter and one number',
+		});
+	}
+	if (!name.match(/^[a-zA-Z ]{2,32}$/)) {
+		return res.status(400).send({
+			success: false,
+			message: 'name must be alphabetic and contain 2-32 characters',
 		});
 	}
 	const admin = await Admin.findOne({
@@ -46,6 +71,7 @@ export default async (req, res) => {
 	});
 	return res.status(200).send({
 		success: true,
+		message: 'Admin added successfully',
 		data: newAdmin,
 	});
 };
