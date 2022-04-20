b19b9bd20f63b46fa5ea0c6bb9b39f9a81502706
Yousef Ashraf <bosayousef981@gmail.com>
2022-04-20 11:28:53 +0200

    add message and validation

--- AdminServer/Routes/admin/subadmins/update.js
+++ AdminServer/Routes/admin/subadmins/update.js
@@ -11,12 +11,61 @@ export default async (req, res) => {
 			message: 'id must be a number',
 		});
 	}
-	if (!adminName && !email && !password && !phoneNumber && !roleId) {
+	if (!adminName || !email || !password || !phoneNumber || !roleId) {
 		return res.status(400).send({
 			success: false,
 			message:
-				'adminName, email, password, phoneNumber, roleId are required',
+				'adminName, email, password, phoneNumber, roleId are optinal but at least one is required',
 		});
+	}
+	if (adminName) {
+		if (!adminName.match(/^[a-zA-Z ]{2,32}$/)) {
+			return res.status(400).send({
+				success: false,
+				message: 'name must be alphabetic and contain 2-32 characters',
+			});
+		}
+	}
+	if (email) {
+		if (
+			!email.match(
+				/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+			)
+		) {
+			return res.status(400).send({
+				success: false,
+				message: 'email is invalid',
+			});
+		}
+	}
+	if (phoneNumber) {
+		if (!phoneNumber.match(/^[0-9]{11}$/)) {
+			return res.status(400).send({
+				success: false,
+				message: 'phoneNumber must be numeric and contains 11 digits',
+			});
+		}
+	}
+	if (password) {
+		if (
+			!password.match(
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/
+			)
+		) {
+			return res.status(400).send({
+				success: false,
+				message:
+					'password must be at least 8 characters, contain at least one uppercase letter, one lowercase letter and one number',
+			});
+		}
+	}
+	if (roleId) {
+		if (isNaN(roleId)) {
+			return res.status(400).send({
+				success: false,
+				message: 'roleId must be a number',
+			});
+		}
 	}
 	const admin = await Admin.findOne({
 		where: {
@@ -44,6 +93,7 @@ export default async (req, res) => {
 	delete updatedAdmin.dataValues.passwordHash;
 	return res.status(200).send({
 		success: true,
+		message: 'Sub-admin updated',
 		data: updatedAdmin,
 	});
 };
