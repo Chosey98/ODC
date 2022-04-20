d625be423a4318212faefcdf4568f769a9b84ef6
Yousef Ashraf <bosayousef981@gmail.com>
2022-04-20 12:33:58 +0200

    validation and message on success

--- AdminServer/Routes/shared/questions/update.js
+++ AdminServer/Routes/shared/questions/update.js
@@ -39,7 +39,94 @@ export default async (req, res) => {
 				'questionContent, questionMark, isTask, taskId, answer1, answer2, answer3, answer4, correctAnswer, examId are required',
 		});
 	}
-
+	if (isTask && !taskId) {
+		return res.status(400).send({
+			success: false,
+			message: 'taskId is required',
+		});
+	}
+	if (!isTask && taskId) {
+		return res.status(400).send({
+			success: false,
+			message: 'isTask is set to false yet there is a taskId',
+		});
+	}
+	if (isTask && taskId && isNaN(taskId)) {
+		return res.status(400).send({
+			success: false,
+			message: 'taskId must be a number',
+		});
+	}
+	if (questionContent) {
+		if (questionContent.length > 500) {
+			return res.status(400).send({
+				success: false,
+				message: 'questionContent must be less than 500 characters',
+			});
+		}
+	}
+	if (questionMark) {
+		if (isNaN(questionMark)) {
+			return res.status(400).send({
+				success: false,
+				message: 'questionMark must be a number',
+			});
+		}
+	}
+	if (answer1) {
+		if (answer1.length > 500) {
+			return res.status(400).send({
+				success: false,
+				message: 'answer1 must be less than 500 characters',
+			});
+		}
+	}
+	if (answer2) {
+		if (answer2.length > 500) {
+			return res.status(400).send({
+				success: false,
+				message: 'answer2 must be less than 500 characters',
+			});
+		}
+	}
+	if (answer3) {
+		if (answer3.length > 500) {
+			return res.status(400).send({
+				success: false,
+				message: 'answer3 must be less than 500 characters',
+			});
+		}
+	}
+	if (answer4) {
+		if (answer4.length > 500) {
+			return res.status(400).send({
+				success: false,
+				message: 'answer4 must be less than 500 characters',
+			});
+		}
+	}
+	if (correctAnswer) {
+		if (
+			correctAnswer !== 'answer1' ||
+			correctAnswer !== 'answer2' ||
+			correctAnswer !== 'answer3' ||
+			correctAnswer !== 'answer4'
+		) {
+			return res.status(400).send({
+				success: false,
+				message:
+					'correctAnswer must be answer1 or answer2 or answer3 or answer4',
+			});
+		}
+	}
+	if (examId) {
+		if (isNaN(examId)) {
+			return res.status(400).send({
+				success: false,
+				message: 'examId must be a number',
+			});
+		}
+	}
 	const question = await Question.findOne({
 		where: {
 			id,
